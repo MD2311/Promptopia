824340e1b557a4cc0af01bb3d70ece94a6ae9533
MD2311 <pmduc2311@gmail.com>
2024-05-30 15:58:09 +0700

    fixing deploy bug 2.2 - mongodb uri

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, Suspense } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -7,7 +7,6 @@ import Form from "@components/Form";
 const UpdatePrompt = () => {
   const router = useRouter();
 
-  // Use useSearchParams() directly
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
@@ -54,15 +53,13 @@ const UpdatePrompt = () => {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Form
-        type="Edit"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-      />
-    </Suspense>
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
   );
 };
 
