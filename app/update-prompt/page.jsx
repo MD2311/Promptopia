f6630dd16aad94191b8defc8dcb4ac26444aa2eb
MD2311 <pmduc2311@gmail.com>
2024-05-30 15:40:27 +0700

    fixing deploy bug 2.1 - suspense boundary

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useEffect, useState, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -54,13 +54,15 @@ const UpdatePrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
